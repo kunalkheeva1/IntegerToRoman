43c91ee4c598a8d5231383646caf9ebf902d1cff
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-06-17 22:58:00 -0700

    new code occupying the same memory according to leetcode but the time has halved. and both codes are easy to grasp now. It is making more sense now

--- src/IntegerToRoman.java
+++ src/IntegerToRoman.java
@@ -15,11 +15,31 @@ public class IntegerToRoman {
                 ones[num%10];                   //2103%10 =3 at 3 index III
     }
 
+    //creating another method to improve the efficiency without using any extra space
+    public static String integerToRoman2(int num){
+        //I will take two arrays of both the numbers
+        int [] intCode = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
+        String[] code= {"M","CM", "D", "CD","C", "XC","L", "XL","X","IX","V","IV","I"};
+
+    //will store the result in the string builder so that i can use append
+        StringBuilder sb = new StringBuilder();
+        //loop thru intCode
+        for(int i=0; i<intCode.length; i++){
+            //runs until the num becomes less than any number in intCode
+            while(num>=intCode[i]){
+                sb.append(code[i]);
+                num = num- intCode[i];
+            }
+        }return sb.toString();
+
+    }
+
 
 
 
     public static void main(String[] args) {
         System.out.println(integerToRoman(2103));
+        System.out.println(integerToRoman2(2103));
 
     }
 }
